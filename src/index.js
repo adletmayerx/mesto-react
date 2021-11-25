fa6b6d21fa9d4cac69e302adb7b3ed5f8c21629b
Artur Sadrtdinov <79194435+asadrtdinov@users.noreply.github.com>
2021-11-25 20:04:47 +0500

    feat: form validation

--- src/index.js
+++ src/index.js
@@ -21,12 +21,7 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// const form = document.querySelector("form[name=edit-profile]");
-// const inputList = Array.from(
-//   form.querySelectorAll('.form__input')
-// );
 
-// console.log(form, inputList);
 const editProfileFormValidator = new FormValidator(
   selectors,
   editProfileFormSelector
